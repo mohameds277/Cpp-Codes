82560394d85cfe07ad8efd6dd9d1e8629434f8c6
mohameds277 <mohameds277@gmail.com>
2023-09-21 12:32:05 +0300

    added new patch

--- Linked_list_DS.cpp
+++ Linked_list_DS.cpp
@@ -1,111 +1,238 @@
-//Work in Progress library of linked-list
 
+//Building block of each node of the linked list 
 #include <iostream>
 
-using std::cin;
 using std::cout;
 using std::endl;
-
-// class linked_list 
-// {
-
-// public :
-//     struct Node
-//     {
-//         int data;
-//         Node *next_ptr;
-
-//         Node (int data ) : data(data) {}                //STRUCT NODE CONSTRUCTOR , DATA ASSIGNED TO THE DATA ABOVE 
-//     };
-
-
-
-
-
-
-// // void create_node(int data)
-// // {
-// //     Node *node_1 = new Node(1);
-// // }
-
-
-// };
-
+using std::swap;
 
 struct Node 
 {
-    int data;
-    Node *next_ptr;
+    int data{} ;                // initialized to zero to prevent garbage value initialization 
+    struct Node *next{};            
 
-    Node(int data_) : data(data_) {} // list constructor initialized list  ( data_member = data_argument )
-};
+    Node(int data ) : data(data) {}     // every time a new node is constructed then 
+};                                      // a node with data and next is nulled like below node
+                                            //  ---------------------
+                                            //  | Data    | next_ptr| --> nullptr 
+                                            //  ---------------------
 
 
-
-void print_list(Node* head_list)        // call by value , pass the pointer value copy ( address value ) into the function , the function works with the given address with no modifications 
+//Linked_list class 
+class Linked_list
 {
-    while(head_list != nullptr)
-    {
-        cout << head_list->data << "\t"; 
-        head_list = head_list->next_ptr;    // update the pointer location into the next node 
-    }
-    cout << endl;
+    private: 
+        Node *head{};           // head pointer of the list , indicates the list , and it's beginning 
 
-}
+                                //          HEAD
+                                //  ---------------------
+                                //  | 0    | next_ptr| --> nullptr 
+                                //  --------------------
 
+        Node *tail{};           // Tail pointer of the list , indicates the last element of the list ,
 
+                                //      Tail
+                                //   --------------------
+                                //  | 0       | next_ptr| --> nullptr 
+                                //   ---------------------
+                                //good for performance instead of
+                                    //traversing the whole list if the target is the rear end of the list 
 
-void print_list(Node* &head_list , bool dummy_par)          //call by reference  , pass the address of the pointer ( the address of the address) !!! CRITICAL FUNCTION !!! BE AWARE
-{                                                                                                                                              // DON'T PLAY WITH FIRE (POINTERS)
-    //uncomment this line to destroy the world 
-    // head_list++;  
+        int list_length{} ;     // new idea : thinking in-progress 
 
-    while(head_list != nullptr)
-    {
-        cout << head_list->data << "\t"; 
-        head_list = head_list->next_ptr;    // update the pointer location into the next node 
-    }
-    cout << endl;
+    public:
+        void print() 
+        {
 
+            Node *temp_node{nullptr};
 
+            temp_node = head ;              // head is very critical , mess with the head 
+                                                    // and the list goes boom 
+                                                    // mess with the head stunt double (temp_head) 
+            while( temp_node != nullptr)
+            {
+                cout << temp_node->data << "->";        // print the current node 
+                                                
+                temp_node = temp_node->next;    // and then update to the next node 
+            }
+            cout << "NULL(end of the list)" ; 
+            cout <<endl;
 
-}
+        }
 
+        void print_method_2()   // more compressed and convenient   print method using for_loop
+        {
+            for (Node *current_node = head; current_node; current_node = current_node->next)
+            {
+                cout << current_node->data << " ";
+
+            }
+            cout << endl; 
+        }
+
+        void append_end(int data)           // only the tail will be used 
+        {
+            Node *new_node = new Node(data);        // created a new node with value data and next
+                                                    // ptr null
+
+            
+            if ( !head )                     // if list is empty ( head points at null)
+                head = tail = new_node;      // then new node will be the head  and the tail 
+                                             // the only one in the list ( the chosen one )
+            else 
+            {
+            tail->next = new_node;      // the current node that the tail is pointing at right now
+                                         // will point it's next to the new node (APPENDING)
+
+            tail = new_node;                // after the append , the  new node will be the last node
+                                            // then it will be the new tail ( all hail the new tail)
+
+            ++list_length;                  // new node added to the gang  -> gang counter incremented                                
+            }                                   
+
+        }
+
+        Node* get_nth_element(int number_position)
+        {
+            int start_counter{};
+            for (Node *current_node{head}; current_node; current_node = current_node->next)
+            {
+                if(start_counter++ == number_position)
+                    return current_node; 
+            }
+            return NULL;
+        }
+
+        void search_by_value(int searched_data) // only print the location based on value , can be modified to return index by replacing
+        {                                                               // cout<<index ; return ;  with return index ; 
+            int index_number{};
+            for (Node *current_node{head}; current_node; current_node = current_node->next , index_number++) 
+            {                                                       // Remember , never mess with the head !!! 
+                if ( current_node->data == searched_data  )
+                {
+                    cout << index_number;
+                    return;
+                }
+ 
+            }
+            cout << "-1";
+        }
+
+        //(optimized solution version 1 ! )
+        void search_by_value_optimized_MK1(int searched_data) // optimized from above method , when value is searched , the node 
+        {                                                    // shifts one left for faster search for the SAME ELEMENT (most requested)
+            int index_number{};
+            Node *previous_node{};      //to keep previous node 
+
+            for (Node *current_node{head}; current_node; current_node = current_node->next, index_number++)
+            { // Remember , never mess with the head !!! 
+                if ( current_node->data == searched_data  )
+                {   
+                    if(!previous_node)  // in case the node reached the head of the list ( no previous node available  )
+                    {                        // when occurs , the rest of the  program will be spared 
+                        cout << index_number;
+                        return;
+                    }
+
+                    swap(previous_node->data, current_node->data) ;  //only the data nodes  swaps , not the whole nodes (optimized solution!)
+                    cout << index_number - 1   ;
+                    return;
+                }
+                previous_node = current_node;   //store the previous node state 
+            }
+            cout << "-1"; 
+        }
+
+        //(optimized solution Version 2 !! )
+        void search_by_value_optimized_MK2(int searched_data) // optimized from above method , when value is searched , the node 
+        {                                                    // shifts one left for faster search for the SAME ELEMENT (most requested)
+            int index_number{};
+
+            for (Node *current_node{head} , *previous_node{nullptr} ; current_node ;  previous_node = current_node  , 
+            current_node = current_node->next ,index_number++ )
+            { // Remember , never mess with the head !!! 
+                if ( current_node->data == searched_data  )
+                {   
+                    if(!previous_node)  // in case the node reached the head of the list ( no previous node available  )
+                    {                        // when occurs , the rest of the  program will be spared 
+                        cout << index_number;
+                        return;
+                    }
+
+                    swap(previous_node->data, current_node->data) ;  //only the data nodes  swaps , not the whole nodes (optimized solution!)
+                    cout << index_number - 1   ;
+                    return;
+                }
+            }
+            cout << "-1"; 
+        }
+
+        void delete_end()
+        {
+            
+            --list_length; 
+        }
+};
 
 
 int main()
 {
 
-    Node *node_0 = new Node(0);             // The list head 
-    Node *node_1 = new Node(1);
-    Node *node_2 = new Node(1);
-    Node *node_3 = new Node(3);
-    Node *node_4 = new Node(4);
+    Linked_list listA ; // new list object is created , this list is called listA  \
+                        // with this list creation we creat head and tail and list_length
 
 
-// Manual linking of the nodes to make a useful list , instead of scattered data in the heap 
-    node_0->next_ptr = node_1;
-    node_1->next_ptr = node_2;
-    node_2->next_ptr = node_3;
-    node_3->next_ptr = node_4;
-    node_4->next_ptr = nullptr;
+    // time to fill it to check it 
 
-    cout << node_0->data << endl;
-
-    //same values , different ways of access 
-    cout << node_1->data << endl;
-    cout << node_0->next_ptr->data << endl; 
+    listA.append_end(1);        // list is empty now ->  head = tail = new_node with value 1 ; 
 
 
-    //same values , different ways of access
-    cout << node_4->data << endl;
-    cout << node_0->next_ptr->next_ptr->next_ptr->next_ptr->data << endl;
+    listA.append_end(2);     
 
-    print_list(node_0);
+    listA.append_end(3);
 
-    print_list(node_0 , true );
+    listA.append_end(4);        
+
+    listA.append_end(5);        
+
+    listA.append_end(6);     
+
+    listA.append_end(534); 
 
 
+    listA.append_end(12134);        
+
+    listA.print();
+    listA.print_method_2();
+
+
+
+    // base linked_list ADT + ending append + printing list : functional and tested.
+
+    
+    Node *temp{};
+    temp = listA.get_nth_element(9); // test for  get_nth_element() method
+    if(!temp)
+        cout << "NULL";
+    else 
+        cout << temp->data;                 // if data returns , position found , else NULL
+    
+
+    cout << endl; 
+
+
+    listA.search_by_value(4);       //print search
+
+    cout << endl;
+
+
+    listA.search_by_value_optimized_MK1(3); // print search optimized (prints the location - 1 )
+
+    cout << endl;
+
+    listA.search_by_value_optimized_MK2(3); // print search optimized (prints the location - 1 , not same as  the above 
+                                                                // method because MK1 was tested before it ) 
+                                                                // don't believe me ? comment it then ! 
 
     return 0;
 }
