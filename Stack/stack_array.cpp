e6963b88113bf45a1002a129709f73bb4fc2cc28
mohameds277 <mohameds277@gmail.com>
2023-12-10 19:04:17 +0200

    starting stack array

---
+++ Stack/stack_array.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+
+
+
+class Stack{
+
+    private : 
+        int size{};
+        int top{};
+        int* array{};
+
+    public :
+        Stack(int size ) : size(size) , top(-1)
+        {
+            array = new int[size];
+        } 
+
+
+
+
+
+
+
+
+
+
+
+};
+
+
+
+
+
+
+
+
+
+int main()
+{
+
+}
